331ac1bbcba292a20e84b54fb5037e3761398d4c
tom.pedron <tom.pedron@digitalalchemy.global>
2020-08-12 16:06:11 -0400

    Task 12 - Tag Update Endpoint

--- app/data_transfer_objects/tag_dto.rb
+++ app/data_transfer_objects/tag_dto.rb
@@ -1,8 +1,9 @@
 class TagDto
-  attr_accessor :name
+  attr_accessor :uuid, :name
 
   def initialize(hash_params)
     attributes = hash_params.dig(:data, :attributes) || {}
+    @uuid = attributes.dig(:id) || hash_params.dig(:id)
 
     @name = attributes.dig(:name)
   end
