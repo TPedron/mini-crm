1ecde3344f796e06f09bd43afdc2c8fc7632b8ff
tom.pedron <tom.pedron@digitalalchemy.global>
2020-08-12 16:30:35 -0400

    Task 14 - Improve README & Add Postman Collection

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
   has_and_belongs_to_many :tags
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   before_create :ensure_uuid # NOTE: work around for https://github.com/rails/rails/issues/34237
 
   # NOTE: Used for serializing tag names as an attribute
