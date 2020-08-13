4f2863ce7da4dd50cca06e0f856cc21abbd603de
tom.pedron <tom.pedron@digitalalchemy.global>
2020-08-13 09:26:26 -0400

    Task 16 - DB Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,69 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding Database'
+
+puts 'Adding Tags'
+
+lead_tag = Tag.find_or_create_by!(
+  name: 'Lead'
+)
+
+high_value_tag = Tag.find_or_create_by!(
+  name: 'HighValue'
+)
+
+finalized_tag = Tag.find_or_create_by!(
+  name: 'Finalized'
+)
+
+stage2_tag = Tag.find_or_create_by!(
+  name: 'Stage2'
+)
+
+low_value_tag = Tag.find_or_create_by!(
+  name: 'LowValue'
+)
+
+churned_tag = Tag.find_or_create_by!(
+  name: 'Churned'
+)
+
+real_estate_tag = Tag.find_or_create_by!(
+  name: 'RealEstate'
+)
+
+puts 'Adding Contacts And Associating Tags'
+
+contact1 = Contact.find_or_create_by!(
+  first_name: 'Andrew',
+  last_name: 'Stevens',
+  email: 'andrew@company.com'
+)
+
+unless contact1.tags.any?
+  contact1.tags << lead_tag
+  contact1.tags << high_value_tag
+  contact1.tags << finalized_tag
+end
+
+contact2 = Contact.find_or_create_by!(
+  first_name: 'Bob',
+  last_name: 'Rodriguez',
+  email: 'bob@yahoo.com'
+)
+
+unless contact2.tags.any?
+  contact2.tags << lead_tag
+  contact2.tags << stage2_tag
+  contact2.tags << low_value_tag
+end
+
+contact3 = Contact.find_or_create_by!(
+  first_name: 'Cathy',
+  last_name: 'Smith',
+  email: 'cathy@lycos.com'
+)
+
+unless contact3.tags.any?
+  contact3.tags << churned_tag
+  contact3.tags << finalized_tag
+  contact3.tags << real_estate_tag
+end
